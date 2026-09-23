5fbb863983c4f8c99db623bf0036f262b4bab5c7
Sérgio Lucas Gomes da Silva Rodrigues <sergio.rod.guapi@gmail.com>
2026-09-23 14:49:43 -0300

    Ajustando a Classe RelatorioFinanceiro.cs

--- RelatorioFinanceiro.cs
+++ RelatorioFinanceiro.cs
@@ -1,23 +1,26 @@
+using System;
+using System.Collections.Generic; // Fornece estruturas de dados genéricas,
+// como List<T>, Dictionary<TKey, TValue> e outras coleções.
+// Neste código, é utilizado para trabalhar com List<Orcamento>.
+using GestaoSalarioOrcamento;
+using PortfyModuloInvestimentos;
+using PortifyModulo1;
+
 namespace AnaliseApresentacao;
+
 public class RelatorioFinanceiro
 {
-    public RelatorioFinanceiro() {}
-    public RelatorioFinanceiro(string cliente, int valores, string tipo_invest, string nome_invest, double valor_invest, int ativos)
-    {
-        Cliente = cliente; //Ver isso sobre o sqlite
-        Valores = valores;
-        Tipo_Invest = tipo_invest; // Renda Fixa e Renda Variável
-        Nome_Invest = nome_invest; // para CDB, LCA, Tesouro Direto, Poupança e outros
-        Valor_Invest = valor_invest;
-        Ativos = ativos;
-    }
-   
-    public string Cliente {get; set;} = "";
-    public int Valores {get; set;}
-    public string Tipo_Invest {get; set;} = "";
-    public string Nome_Invest {get; set;} = "";
-    public double Valor_Invest {get; set;}
-    public int Ativos {get; set;}
+    private readonly Usuario _usuario;
+    private readonly CarteiraSimulada _carteira;
+    private readonly List<Orcamento> _orcamentos;
 
-// Quando surgir novas informações, irei adicionar novos métodos e/ou modifica-los
+    public RelatorioFinanceiro(
+        Usuario usuario,
+        CarteiraSimulada carteira,
+        List<Orcamento> orcamentos)
+    {
+        _usuario = usuario ?? throw new ArgumentNullException(nameof(usuario));
+        _carteira = carteira ?? throw new ArgumentNullException(nameof(carteira));
+        _orcamentos = orcamentos ?? new List<Orcamento>();
+    }
 }
