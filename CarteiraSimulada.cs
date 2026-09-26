c8fba104d075ffe67e2a8edb996c48cd9484e9cb
Miguel <124815875+MexicanoRGB@users.noreply.github.com>
2026-09-26 15:57:08 -0300

    Clean up namespaces in CarteiraSimulada.cs
    Removed unused namespaces from CarteiraSimulada.cs

--- CarteiraSimulada.cs
+++ CarteiraSimulada.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using SimuladorFinanceiro;
-using PortfyModuloInvestimentos;
-
-namespace PortfyModuloInvestimentos;
+namespace Portfy.Dominio.Investimentos;
 
 public class CarteiraSimulada
 {
